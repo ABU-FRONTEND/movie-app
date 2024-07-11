0bb32aaf4a090cc94e643173fa21e99040e57f1e
Shohjahon-n <pazvat56@gmail.com>
2024-07-11 23:12:23 +0500

    back-end finished

--- back-end/index.js
+++ back-end/index.js
@@ -9,12 +9,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 const connectUrl = "mongodb+srv://shojik:3NAqlYto5jmCQcTj@backend.6l8ntax.mongodb.net/?retryWrites=true&w=majority&appName=backend";
 
+import data from './data.js';
+
 app.use(cors());
 app.use(express.json());
-
-mongoose.connect(connectUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+function connectDB() {
+    mongoose.connect(connectUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log('MongoDB connected'))
+        .catch(err => console.error('MongoDB connection error:', err));
+}
 
 app.post('/register', async (req, res) => {
     const { username, email, password } = req.body;
@@ -59,20 +62,50 @@ app.post('/login', async (req, res) => {
     }
 });
 
-app.get('/protected', verifyToken, (req, res) => {
-    res.status(200).json({ message: 'Protected route accessed successfully' });
+app.post('/bookmarks', verifyToken, async (req, res) => {
+    const userId = req.user._id;
+    const { movieId } = req.body;
+    console.log(userId, movieId);
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        if (!user.bookmarks.includes(movieId)) {
+            user.bookmarks.push(movieId);
+            await user.save();
+            return res.status(200).json({ message: 'Movie added to bookmarks' });
+        } else {
+            return res.status(400).json({ message: 'Movie already in bookmarks' });
+        }
+    } catch (error) {
+        console.error('Bookmark error:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 });
 
 app.get('/logout', (req, res) => {
     res.status(200).json({ message: 'Logout successful' });
 });
 
-app.get('/all', (req, res) => {
-    res.status(200).json({ message: 'All users retrieved successfully' });
+app.get('/search', (req, res) => {
+    const query = req.query.query.toLowerCase();
+    res.status(200).json(data.filter(movie => movie.title.toLowerCase().includes(query)));
 });
 
-app.get('/category:category', (req, res) => {
-    
+app.get('/all', (req, res) => {
+    res.status(200).json(data);
+});
+
+app.get('/popular', (req, res) => {
+    res.status(200).json(data.filter(movie => movie.isTrending));
+})
+
+app.get('/category/:category', (req, res) => {
+    const category = req.params.category.toLowerCase();
+    let filteredData = data.filter(movie => movie.category.toLowerCase() === category);
+    res.status(200).json(filteredData);
 });
 
 function verifyToken(req, res, next) {
@@ -94,4 +127,5 @@ function verifyToken(req, res, next) {
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
+    connectDB();
 });
